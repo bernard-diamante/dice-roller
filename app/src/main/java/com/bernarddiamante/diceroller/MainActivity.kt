1108d3c3fdfbc4b8c88dd01d9950c262d7dbccd8
Bernard Diamante <bernardiamante@gmail.com>
2022-09-04 19:15:22 +0800

    Dice images change after rolling

--- app/src/main/java/com/bernarddiamante/diceroller/MainActivity.kt
+++ app/src/main/java/com/bernarddiamante/diceroller/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.bernarddiamante.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bernarddiamante.diceroller.databinding.ActivityMainBinding
-import kotlin.properties.Delegates
+
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        const val TAG = "MainActivity.kt"
-        const val numSides = 6
-    }
     private lateinit var binding: ActivityMainBinding
-    private var result by Delegates.notNull<Boolean>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,29 +24,40 @@ class MainActivity : AppCompatActivity() {
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
 
-            val rollResult1 = rollDice()
-            binding.tvRollNumber1.text = rollResult1.toString()
-            val rollResult2 = rollDice()
-            binding.tvRollNumber2.text = rollResult2.toString()
+            rollDice(binding.ivDice1)
+            rollDice(binding.ivDice2)
 
-            when {
-                rollResult1 > rollResult2 -> binding.tvResult.text = ">:D"
-                rollResult1 < rollResult2 -> binding.tvResult.text = ":'("
-                rollResult1 == rollResult2 -> binding.tvResult.text = ":/"
-            }
         }
     }
 
-    private fun rollDice(): Int {
-        val dice = Dice(numSides)
-        return dice.roll()
+    /*
+    * Roll the dice and update the dice images.
+    * */
+    private fun rollDice(ivDice: ImageView) {
+        // Create new Dice object with x number of sides and roll
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
 
+        // Select image based on roll result
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Display dice image
+        ivDice.setImageResource(drawableResource)
+        ivDice.contentDescription = diceRoll.toString()
     }
-}
 
-class Dice(private val numSides: Int) {
 
-    fun roll(): Int {
-        return (1..numSides).random()
+    class Dice(private val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
     }
 }
